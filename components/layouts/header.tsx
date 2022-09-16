09129a4155ca30ca3f113d5381cdccb79509fa58
cryptodev26 <cryptodev26@gmail.com>
2022-09-16 05:30:50 +0300

    fix error

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,4 +1,4 @@
-import { StayPrimaryLandscapeSharp } from '@mui/icons-material'
+
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
